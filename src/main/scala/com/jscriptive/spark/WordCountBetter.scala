345d549075ebe3fe520d854d13a8b3044bdab033
Gerd Reiss <gerdreiss@jscriptive.com>
2017-03-26 13:59:28 +0200

    better word count

---
+++ src/main/scala/com/jscriptive/spark/WordCountBetter.scala
@@ -0,0 +1,33 @@
+package com.jscriptive.spark
+
+import org.apache.log4j._
+import org.apache.spark._
+
+/** Count up how many of each word occurs in a book, using regular expressions. */
+object WordCountBetter {
+
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine
+    val sc = new SparkContext("local[*]", "WordCountBetter")
+
+    // Load each line of my book into an RDD
+    val input = sc.textFile("book.txt")
+
+    // Split using a regular expression that extracts words
+    val words = input.flatMap(x => x.split("\\W+"))
+
+    // Normalize everything to lowercase
+    val lowercaseWords = words.map(x => x.toLowerCase())
+
+    // Count of the occurrences of each word
+    val wordCounts = lowercaseWords.countByValue()
+
+    // Print the results
+    wordCounts.foreach(println)
+  }
+}
