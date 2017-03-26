7f2888eda646645f49c6e7fce4541db402887bbf
Gerd Reiss <gerdreiss@jscriptive.com>
2017-03-26 14:38:11 +0200

    word count and length, sorted

---
+++ src/main/scala/com/jscriptive/spark/WordCountAndLength.scala
@@ -0,0 +1,32 @@
+package com.jscriptive.spark
+
+import org.apache.log4j._
+import org.apache.spark._
+
+/** Count up how many of each word occurs in a book, using regular expressions and sorting the final results */
+object WordCountAndLength {
+
+  case class Numbers(count: Int, length: Int)
+
+  case class Word(word: String, numbers: Numbers) {
+    override def toString: String = f"${word.padTo(18, " ").mkString}%s ${numbers.length}%6d ${numbers.count}%4d"
+  }
+
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using the local machine
+    val sc = new SparkContext("local", "WordCountBetterSorted")
+
+    sc.textFile("book.txt")
+      .flatMap(_.split("\\W+"))
+      .map(_.toLowerCase())
+      .map(word => (word, Numbers(1, word.length)))
+      .reduceByKey((data1, data2) => Numbers(data1.count + data2.count, data1.length))
+      .map { case (word, nums) => (nums.count, Word(word, nums)) }.sortByKey()
+      .foreach(result => println(s"${result._2}"))
+  }
+}
