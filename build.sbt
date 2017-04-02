6d9f66ee5072c77adf86b145d7a9a69e7492a482
Gerd Reiss <gerdreiss@jscriptive.com>
2017-04-02 13:45:15 +0200

    parsing real world logs -> alarms, cli args, idle server alarm

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ scalaVersion := "2.11.8"
 
 libraryDependencies ++= {
   val sparkVersion = "2.1.0"
+  val scallopVersion = "2.1.1"
 
   Seq(
     // Apache Spark
@@ -15,6 +16,7 @@ libraryDependencies ++= {
     "org.apache.spark" %% "spark-sql" % sparkVersion,
     "org.apache.spark" %% "spark-streaming" % sparkVersion,
     "org.apache.bahir" %% "spark-streaming-twitter" % sparkVersion,
+    "org.rogach" %% "scallop" % scallopVersion,
     // Config
     "com.typesafe" % "config" % "1.3.1"
   )
--- src/main/scala/com/jscriptive/spark/streaming/LogAlarmer.scala
+++ src/main/scala/com/jscriptive/spark/streaming/LogAlarmer.scala
@@ -2,14 +2,36 @@ package com.jscriptive.spark.streaming
 
 import com.jscriptive.spark.streaming.Utilities._
 import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext, Time, Duration}
+import org.rogach.scallop.{ScallopConf, ScallopOption}
+
+import scala.util.Try
 
 /** Monitors a stream of Apache access logs on port 9999, and prints an alarm
   * if an excessive ratio of errors is encountered.
   */
 object LogAlarmer {
 
+  class Conf(args: Seq[String]) extends ScallopConf(args) {
+    val host: ScallopOption[String]      = opt[String](default = Some("127.0.0.1"))
+    val port: ScallopOption[Int]         = opt[Int](default = Some(9999))
+    val windowLength: ScallopOption[Int] = opt[Int](default = Some(300))
+    val windowSlide: ScallopOption[Int]  = opt[Int](default = Some(1))
+    val maxIdle: ScallopOption[Long]     = opt[Long](default = Some(1800))
+    val maxRatio: ScallopOption[Double]  = opt[Double](default = Some(0.5))
+    verify()
+  }
+
   def main(args: Array[String]) {
+
+    val cliConf: Conf     = new Conf(args)
+    val host: String      = cliConf.host()
+    val port: Int         = cliConf.port()
+    val windowLength: Int = cliConf.windowLength()
+    val windowSlide: Int  = cliConf.windowSlide()
+    val maxIdle: Duration = Duration(cliConf.maxIdle() * 1000L)
+    val maxRatio: Double  = cliConf.maxRatio()
+
 
     // Create the context with a 1 second batch size
     val ssc = new StreamingContext("local[*]", "LogAlarmer", Seconds(1))
@@ -40,8 +62,10 @@ object LogAlarmer {
     //// Tally up statuses over a 5-minute window sliding every second
     //val statusCounts = successFailure.countByValueAndWindow(Seconds(300), Seconds(1))
 
+    var t0 = Time(System.currentTimeMillis())
+
     // Better version of the above
-    ssc.socketTextStream("127.0.0.1", 9999, StorageLevel.MEMORY_AND_DISK_SER)
+    ssc.socketTextStream(host, port, StorageLevel.MEMORY_AND_DISK_SER)
       .map(pattern.matcher(_))
       .filter(_.matches())
       .map(_.group(6))
@@ -51,9 +75,9 @@ object LogAlarmer {
         else if (status >= 500 && status < 600) "Failure"
         else "Other"
       })
-      .countByValueAndWindow(Seconds(300), Seconds(1))
+      .countByValueAndWindow(Seconds(windowLength), Seconds(windowSlide))
       // For each batch, get the RDD's representing data from our current window
-      .foreachRDD((rdd, _) => {
+      .foreachRDD((rdd, now) => {
 
       // Keep track of total success and error codes from each RDD
       // var totalSuccess: Long = 0
@@ -74,25 +98,35 @@ object LogAlarmer {
 
       // Better way to compute the totals
       val res: Map[String, Long] = rdd.reduceByKey(_ + _).collect().toMap
+
       val totalSuccess: Long = if (res.isEmpty) 0 else res("Success")
       val totalError: Long = if (res.isEmpty) 0 else res("Failure")
 
       // Print totals from current window
       println("Total success: " + totalSuccess + " Total failure: " + totalError)
 
+
       // Don't alarm unless we have some minimum amount of data to work with
-      if (totalError + totalSuccess > 100) {
-        // Compute the error rate
-        // Note use of util.Try to handle potential divide by zero exception
-        val ratio: Double = util.Try(totalError.toDouble / totalSuccess.toDouble) getOrElse 1.0
-        // If there are more errors than successes, wake someone up
-        if (ratio > 0.5) {
-          // In real life, you'd use JavaMail or Scala's courier library to send an
-          // email that causes somebody's phone to make annoying noises, and you'd
-          // make sure these alarms are only sent at most every half hour or something.
-          println("Wake somebody up! Something is horribly wrong.")
-        } else {
-          println("All systems go.")
+      if (totalError + totalSuccess == 0) {
+        val idle: Duration = now - t0
+        if (idle > maxIdle) {
+          println("Wake somebody up! The server seems down.")
+        }
+      } else {
+        t0 = now
+        if (totalError + totalSuccess > 100) {
+          // Compute the error rate
+          // Note use of util.Try to handle potential divide by zero exception
+          val ratio: Double = Try(totalError.toDouble / totalSuccess.toDouble) getOrElse 1.0
+          // If there are more errors than successes, wake someone up
+          if (ratio > maxRatio) {
+            // In real life, you'd use JavaMail or Scala's courier library to send an
+            // email that causes somebody's phone to make annoying noises, and you'd
+            // make sure these alarms are only sent at most every half hour or something.
+            println("Wake somebody up! Something is horribly wrong.")
+          } else {
+            println("All systems go.")
+          }
         }
       }
     })
@@ -107,4 +141,3 @@ object LogAlarmer {
     ssc.awaitTermination()
   }
 }
-
